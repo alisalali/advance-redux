01b6fddbc04a710b39ba1c04c665f835c4acf592
alisalali <alis.alali@hotmail.com>
2024-01-15 10:40:59 +0300

    finalize fetching logic , changed flag property

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -17,13 +17,18 @@ export const fetchCartData = () => {
     };
     try {
       const cartData = await fetchData();
-      dispatch(cartActions.replaceCart(cartData));
+      dispatch(
+        cartActions.replaceCart({
+          items: cartData.items || [],
+          totalQuantity: cartData.totalQuantity || 0,
+        })
+      );
     } catch (error) {
       dispatch(
         uiActions.showNotification({
           status: "error",
           title: "Error",
-          message: "Sending data failed",
+          message: " Fetching data failed",
         })
       );
     }
@@ -45,7 +50,10 @@ export const sendCartData = (cart) => {
         "https://redux-food-order-fd94b-default-rtdb.firebaseio.com/cart.json",
         {
           method: "PUT",
-          body: JSON.stringify(cart),
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
       );
 
